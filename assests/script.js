362783cfb7ea63f441d8792f141e71f05246782e
Jeff Morrow <MorrowJefferyA@gmail.com>
2020-02-05 12:23:42 -0800

    combined both scripts and it sorta works now

--- assests/script.js
+++ assests/script.js
@@ -1,21 +1,21 @@
 //globals
 var wrapper = document.querySelector(".wrapper");
 var footer = document.querySelector(".footer");
-var searchTerm = "election"; // this will get user input later
-var numofRec = 5; // this will get user input later
 
-// // https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey
+  $('.searchButton').on('click', function(event){
+    event.preventDefault()
+    var searchTerm = $('.searchTerm').val();
+    var recordCount = $('#recordCount').val();
+    addArticles(searchTerm, recordCount);
+  })
 
-// queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=JjjjUGVZqjMXl1fACexkIcrBmgjZKslq";
 
-
-$.ajax({
+  function addArticles(searchTerm, numofRec){
+    queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=JjjjUGVZqjMXl1fACexkIcrBmgjZKslq";
+  $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-     console.log(response.response.docs[0])
-     console.log(response.response.docs[0].headline.main);
-     console.log(response.response.docs[0].abstract);
 
     for (var i = 0; i < numofRec; i++) {
         var container = document.createElement("div");
@@ -33,3 +33,4 @@ $.ajax({
         containerBody.appendChild(para);
     }
   })
+}
